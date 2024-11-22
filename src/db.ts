1a942255d54a1993107b6883f5d17d038f875b41
ajaychelikhani <ajaychelikhani@gmail.com>
2024-11-23 01:34:21 +0530

    Completed partial signup endpoint

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,11 @@
-imp
+import mongoose,{model,Schema} from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/secondbrain15")
+
+const UserSchema=new Schema({
+    username:{type:String,unique:true},
+    password:String
+})
+
+
+export const UserModel=model("User",UserSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { UserModel } from "./db";
 
 const app=express();
+app.use(express.json());
 
-app.post("/api/v1/signup",(req,res)=>{
-
+app.post("/api/v1/signup",async(req,res)=>{
+    //zod validation,Hash the password
+    const username=req.body.username;
+    const password=req.body.password;
+    await UserModel.create({
+        username:username,
+        password:password,
+    })
+    res.json({
+        message:"User signed up"
+    })
 })
 
 app.post("/api/v1/signin",(req,res)=>{
