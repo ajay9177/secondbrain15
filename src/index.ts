b08bf47aba1a8d871166c940f627e726fcfdb547
ajaychelikhani <ajaychelikhani@gmail.com>
2024-11-23 02:03:08 +0530

    Completed sigin endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,46 @@ import { UserModel } from "./db";
 
 const app=express();
 app.use(express.json());
+const JWT_PASSWORD="testing"
 
 app.post("/api/v1/signup",async(req,res)=>{
     //zod validation,Hash the password
     const username=req.body.username;
     const password=req.body.password;
-    await UserModel.create({
-        username:username,
-        password:password,
-    })
-    res.json({
-        message:"User signed up"
-    })
+    try{
+        await UserModel.create({
+            username:username,
+            password:password,
+        })
+        res.json({
+            message:"User signed up"
+        })
+    }catch(e){
+        res.status(411).json({
+            message:"User already exists"
+        })
+    }
 })
 
-app.post("/api/v1/signin",(req,res)=>{
-    
+app.post("/api/v1/signin",async(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if(existingUser){
+        const token=jwt.sign({
+            id: existingUser._id
+        },JWT_PASSWORD)
+        res.json({
+            token
+        })
+    }else{
+        res.status(403).json({
+            message:"Incorrect credentials"
+        })
+    }
 })
 
 app.post("/api/v1/content",(req,res)=>{
@@ -30,3 +54,6 @@ app.post("/api/v1/content",(req,res)=>{
 app.get("/api/v1/signup",(req,res)=>{
     
 })
+
+
+app.listen(3000);
